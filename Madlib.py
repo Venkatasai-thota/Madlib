a9d3a86e8c1ffdd65dd945881b246e2584a49dc7
Sai Thota <60151953+Venkatasai-thota@users.noreply.github.com>
2022-06-28 00:18:54 -0500

    Add files via upload

---
+++ Madlib.py
@@ -0,0 +1,12 @@
+name=input("What is you name:- ")
+planet=input("Name a planet:- ")
+number=input("Type in your lucky number which is more than one:- ")
+adjective=input("type exicted:- ")
+adjective1=input("Thin or Thick:- ")
+mom_dad=input("Who do you love the most, is it you mom or your dad? type mom if you love your mom the most or type dad if you love your dad the most, type mom and dad if you love both of them the most ")
+madlib= f"Hello my name is astronut {name}!\
+   i am on my way to {planet}\
+    I will be gone for {number} days, i am very much {adjective} about the trip but i will miss my {mom_dad}.\
+         I have heard that the atmosphere there is very {adjective1}.\
+             " 
+print(madlib)
